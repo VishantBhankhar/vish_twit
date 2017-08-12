8d8d6c8eb78155b57284f197d62b59709b888bc7
VishantBhankhar <vishantbhankhar@gmail.com>
2017-08-12 23:44:15 +0530

    error

--- app/init.php
+++ app/init.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once '../vendor/autoload.php';
+require_once 'vendor/autoload.php';
 require_once 'classes/auth.php';
 
 \Codebird\Codebird::setConsumerKey('JJRkcJSwzl7fAAdvJaXEvadbq','RB3UZ64hpnqGS2wIVkjlYQyQCyXd3089BUWQIReEfY8NEihQXu');
