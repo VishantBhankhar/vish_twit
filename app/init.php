64ff4c0ad2f05a42ee274d099607cc201115d126
VishantBhankhar <vishantbhankhar@gmail.com>
2017-08-12 22:27:38 +0530

    Changes error

--- app/init.php
+++ app/init.php
@@ -4,6 +4,6 @@ session_start();
 
 require_once 'vendor/autoload.php';
 
-\Codebird\Codebird::setCustomKey('JJRkcJSwzl7fAAdvJaXEvadbq','RB3UZ64hpnqGS2wIVkjlYQyQCyXd3089BUWQIReEfY8NEihQXu');
+\Codebird\Codebird::setConsumerKey('JJRkcJSwzl7fAAdvJaXEvadbq','RB3UZ64hpnqGS2wIVkjlYQyQCyXd3089BUWQIReEfY8NEihQXu');
 $client = \Codebird\Codebird::getInstance();
 var_dump($client);
