ee72e5346116442a48ef9607a6dd45032de0a03d
VishantBhankhar <vishantbhankhar@gmail.com>
2017-08-13 10:59:31 +0530

    eror

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $auth = new auth($client);
 <?php if($auth->signedIn()): ?>
     <p>You are signed in.</p>
     <?php
-    $this->client->setToken($_SESSION['oauth_token'],$_SESSION['oauth_token_secret']);
+    $client->setToken($_SESSION['oauth_token'],$_SESSION['oauth_token_secret']);
     $reply = (array) $client->statuses_homeTimeline();
     print_r($reply);
     ?>
