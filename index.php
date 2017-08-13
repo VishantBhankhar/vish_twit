240da3c918e741acf92639e69c11c0ee79e37e05
VishantBhankhar <vishantbhankhar@gmail.com>
2017-08-13 10:10:09 +0530

    check twit

--- index.php
+++ index.php
@@ -5,7 +5,12 @@ $auth = new auth($client);
 ?>
 
 <?php if($auth->signedIn()): ?>
-        <p>You are signed in.<a href="signout.php">Sign out</a> </p>
+    <p>You are signed in.</p>
+    <?php
+        $reply=(array)$auth->statuses_omeTimeline();
+        print_r($reply);
+    ?>
+    <p><a href="signout.php">Sign out</a> </p>
 <?php else : ?>
     <p><a href="<?php echo $auth->getAuthUrl();?>">Sign in with Twitter</a> </p>
 <?php endif; ?>
