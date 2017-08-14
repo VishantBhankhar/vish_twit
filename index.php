1691f18f2b33475f0d16a0bc0fe05e85c8ca152d
VishantBhankhar <vishantbhankhar@gmail.com>
2017-08-14 21:41:45 +0530

    check

--- index.php
+++ index.php
@@ -66,8 +66,30 @@ $auth = new auth($client);
     ?>
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="..." alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="..." alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="..." alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <!--<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
             <div class="carousel-item active"
-    <?php
+    <?php/*
     foreach ($twit as $value) {
         $count++;
         if($count>10)
@@ -96,11 +118,11 @@ $auth = new auth($client);
         <?php
             }
         }
-    };
+    };*/
     ?>
 
             </div>
-        </div>
+        </div>-->
 
     <?php
     /*echo 'othertweets'.'<br>';
