8fe5d9359a0064982ebb3e7e9642d5c1000ed2b9
VishantBhankhar <vishantbhankhar@gmail.com>
2017-08-14 21:49:17 +0530

    tggv

--- index.php
+++ index.php
@@ -86,6 +86,15 @@ $auth = new auth($client);
         </a>
     </div>
 
+    <div class="card" style="width: 20rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div
+
     <!--<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active"
