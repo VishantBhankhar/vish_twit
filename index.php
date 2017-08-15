03eb02f002fa7b26dc1cf872e348a3bbbfc979bf
VishantBhankhar <vishantbhankhar@gmail.com>
2017-08-15 14:48:27 +0530

    image dimension check

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ $auth = new auth($client);
     <div class="row">
         <div class="col-sm-6">
             <div class="card" style="width: 20rem;">
-                <img id="image profile" class="card-img-top" src="<?php echo $user_profile_pic ?>" alt="Card image cap">
+                <img id="image_profile" class="card-img-top" src="<?php echo $user_profile_pic ?>" alt="Card image cap">
                 <div class="card-body">
                     <h4 class="card-title">My tweets</h4>
                 </div>
