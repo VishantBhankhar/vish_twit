ed12f4badc376d7b9a4730f5c86cb95bef65d7d1
VishantBhankhar <vishantbhankhar@gmail.com>
2017-08-18 12:46:25 +0530

    check css

--- index.php
+++ index.php
@@ -145,15 +145,17 @@ $auth = new auth($client);
     echo '<br>' . '<br>';
     ?>
     <!--Search Follower-->
-    <div style="float: none" class="row">
-        <input type="text" class="form-control" placeholder="Search Follower Here" list="followerlist">
-        <datalist id="followerlist">
-            <?php foreach ($friend as $key) { ?>
-                <option value="<?php echo $key->name ?>"><a href="https://www.goolge.com"><img
-                                src="<?php echo $key->profile_image_url_https ?>" height="40"
-                                width="30"><?php echo $key->name ?></option></a>
-            <?php } ?>
-        </datalist>
+    <div class="row">
+        <div style="float: none" class="row">
+            <input type="text" class="form-control" placeholder="Search Follower Here" list="followerlist">
+            <datalist id="followerlist">
+                <?php foreach ($friend as $key) { ?>
+                    <option value="<?php echo $key->name ?>"><a href="https://www.goolge.com"><img
+                                    src="<?php echo $key->profile_image_url_https ?>" height="40"
+                                    width="30"><?php echo $key->name ?></option></a>
+                <?php } ?>
+            </datalist>
+        </div>
     </div>
 
     <!-- User's tweets-->
