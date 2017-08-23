20a1741fb79ef0a09644f877d2df35cd7c94a14a
VishantBhankhar <vishantbhankhar@gmail.com>
2017-08-23 22:07:41 +0530

    same

--- pdfDownload.php
+++ pdfDownload.php
@@ -4,27 +4,28 @@ require_once './app/init.php';
 $searchname=$_POST['search_name'];
 $client->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
 $usernamearr=(array)$client->users_lookup('user_id='.$_SESSION['user_id']);
-$personinfo=(array)$client->users_show('screen_name='.$searchname);
+$personinfoarr=(array)$client->users_lookup('screen_name='.$searchname);
+/*
 print "<pre>";
 print_r($usernamearr);
 print "</pre>";
-
+*/
 foreach ($usernamearr as $value) {
     $username=$value->name;
     break;
 };
-
-
-echo $username;
-
+foreach ($personinfoarr as $value) {
+    $personinfo=$value->name;
+    break;
+};
 
 $pdf=new FPDF();
 $pdf->AddPage();
 $pdf->SetFont("Arial","B",20);
-$pdf->Write('','Thank You '.$username->name.' for using my website.');
+$pdf->Write('','Thank You '.$username.' for using my website.');
 $pdf->Ln();
 $pdf->SetFont("Arial","B",16);
-$pdf->Write('','Here is '.$personinfo->name.'\'s tweets.');
+$pdf->Write('','Here is '.$personinfo.'\'s tweets.');
 $pdf->Output();
 /**
  * Created by PhpStorm.
