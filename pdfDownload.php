a1b0b71e58f1f452e40be91f6c5c383c75a700b4
VishantBhankhar <vishantbhankhar@gmail.com>
2017-08-23 23:30:10 +0530

    check for length in pdf

--- pdfDownload.php
+++ pdfDownload.php
@@ -39,10 +39,10 @@ $pdf->Ln(10);
 $pdf->SetFont("Arial","I",8);
 foreach ($tweets as $value) {
     if(isset($value->text)){
-        $pdf->Cell(40,10,$value->id,1,0,'C');
-        $pdf->Cell(100,10,$value->text,1,0,'C');
-        $pdf->Cell(50,10,$value->created_at,1,0,'C');
-        $pdf->Ln(10);
+        $pdf->Cell(40,16,$value->id,1,0,'C');
+        $pdf->Cell(100,16,$value->text,1,0,'C');
+        $pdf->Cell(50,16,$value->created_at,1,0,'C');
+        $pdf->Ln(16);
     }
     else break;
 };
