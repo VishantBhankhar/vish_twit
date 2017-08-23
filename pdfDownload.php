605f91810877eedb32dc5107ec0ae2db70b1cb82
VishantBhankhar <vishantbhankhar@gmail.com>
2017-08-23 21:29:42 +0530

    check

--- pdfDownload.php
+++ pdfDownload.php
@@ -3,6 +3,7 @@ require 'lib/fpdf/fpdf.php';
 require_once './app/init.php';
 $searchname=$_POST['search_name'];
 $client->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
+$username=(array)$client->users_lookup('user_id='.$_SESSION['user_id']);
 $personinfo=(array)$client->users_show('screen_name='.$searchname);
 
         print "<pre>";
@@ -11,8 +12,10 @@ $personinfo=(array)$client->users_show('screen_name='.$searchname);
 
 $pdf=new FPDF();
 $pdf->AddPage();
-$pdf->SetFont("Arial","I",20);
-$pdf->Write('Hello','My name is '.$searchname);
+$pdf->SetFont("Arial","B",20);
+$pdf->Write('','Thank You '.$username->name.' for using my website.');
+$pdf->SetFont("Arial","B",16);
+$pdf->Write('','Here is '.$personinfo->name.'\'s tweets.');
 $pdf->Output();
 /**
  * Created by PhpStorm.
