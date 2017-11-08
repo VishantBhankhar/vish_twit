904ccd862d64cac83310dada7e195157299766f2
VishantBhankhar <vishantbhankhar@gmail.com>
2017-11-08 19:08:59 +0530

    checck

--- app/init.php
+++ app/init.php
@@ -5,7 +5,6 @@ session_start();
 require_once 'vendor/autoload.php';
 require_once 'classes/auth.php';
 
-\Codebird\Codebird::setConsumerKey('xhB4nmeg4c6BpreVwRqiVs3go','bSdUYJoazVFsjvnqhLfwIbKJAfUmZRJeSSvZByVEkscUcSTMna
-');
+\Codebird\Codebird::setConsumerKey('xhB4nmeg4c6BpreVwRqiVs3go','bSdUYJoazVFsjvnqhLfwIbKJAfUmZRJeSSvZByVEkscUcSTMna');
 $client = \Codebird\Codebird::getInstance();
 
