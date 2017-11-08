0a99b1ef13bf843230f9e45442fb1c1b552dd045
VishantBhankhar <vishantbhankhar@gmail.com>
2017-11-08 19:06:48 +0530

    Check

--- app/init.php
+++ app/init.php
@@ -5,6 +5,7 @@ session_start();
 require_once 'vendor/autoload.php';
 require_once 'classes/auth.php';
 
-\Codebird\Codebird::setConsumerKey('JJRkcJSwzl7fAAdvJaXEvadbq','RB3UZ64hpnqGS2wIVkjlYQyQCyXd3089BUWQIReEfY8NEihQXu');
+\Codebird\Codebird::setConsumerKey('xhB4nmeg4c6BpreVwRqiVs3go','bSdUYJoazVFsjvnqhLfwIbKJAfUmZRJeSSvZByVEkscUcSTMna
+');
 $client = \Codebird\Codebird::getInstance();
 
